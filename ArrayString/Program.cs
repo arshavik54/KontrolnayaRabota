dfee9de3ceb02e886c9a4f39f7674f7ba7b18209
Igor <arshavik54@rambler.ru>
2023-11-28 12:56:09 +0500

    решил задачу

--- ArrayString/Program.cs
+++ ArrayString/Program.cs
@@ -5,16 +5,21 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
-Console.Clear();
-int n;
-string[] arr;
-string[] array3str;
 
-Console.Write("Введите колличество элементов: ");
+
+Console.Clear();
+int n;  // колличество элементов в начальном массиве
+string[] arr; // начальный массив
+string[] arr1;  // результат
+
+Console.Write("Введите колличество элементов: ");  
 n = int.Parse(Console.ReadLine());
 
+
 arr = new string[n];
-array3str = new string[n];
+arr1 = new string[n];
+
+
 Console.WriteLine("Введите массив: ");
 
 for (int i = 0; i < arr.Length; i++) 
@@ -22,30 +27,31 @@ for (int i = 0; i < arr.Length; i++)
    Console.Write($"arr[{i}] = ");
    arr[i] = Console.ReadLine();
 }
- 
-void CreateArray(string[] arr, string[] array)
+
+string[] CreateArray(string[] array, string[] newarr)
 {
     int j = 0;
-    for (int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        if (arr[i].Length <= 3)
+    if(array[i].Length <= 3)
         {
-            array[j] = arr[i];
-            j++;
+        newarr[j] = array[i];
+        j++;
         }
     }
+    return newarr;
 }
 
-void PrintArray(string[] array1)
-{
-    for (int i = 0; i < array1.Length; i++)
-    {
-        Console.Write($"{array1[i]},");
-    }
-    Console.WriteLine();
-}   
+string[] now = CreateArray(arr,arr1);
+var strnow = string.Join(",", now);
+var strarr = string.Join(", ", arr);
+Console.Write('[');
+Console.Write(strarr);
+Console.Write(']');
+Console.Write('-');
+Console.Write('>');
 
-CreateArray(arr, array3str);
-Console.WriteLine();
-PrintArray(arr);
-PrintArray(array3str);
+Console.Write('[');
+Console.Write(strnow);
+Console.Write(']');
+
